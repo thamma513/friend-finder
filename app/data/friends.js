cf4f38d65bba1bf5e3337b221d0b3b530547f5f3
thamma513 <thamma513@gmail.com>
2018-06-05 18:50:03 -0600

    ..

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friendsList = [
+
+	{
+		name: "Tyler", 
+		photo: "http://static.nanopress.it/r/845X0/www.nanopress.it/wp-content/uploads/2014/08/Monnalisa1.jpg",
+		scores: [
+
+				"4",
+				"2",
+				"5",
+				"1",
+				"3",
+				"2",
+				"2",
+				"1",
+				"3",
+				"2"
+		]
+	}, {
+		name: "Some dude",
+		photo: "http://imgur.com/DJ",
+		scores: [
+
+				"5",
+				"2",
+				"2",
+				"2",
+				"4",
+				"1",
+				"3",
+				"2",
+				"5",
+				"5"
+		]
+	}, {
+		name: "Marky Mark",
+		photo: "http://image.jpg",
+		scores: [
+
+				"3",
+				"3",
+				"4",
+				"2",
+				"2",
+				"1",
+				"3",
+				"2",
+				"2",
+				"3"
+		]
+	}
+];
+
+module.exports = friendsList;
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require('path');
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
